66891bcca61d39f0520006b5313d98d057365618
W.H.P.Fernando <95269066+Hima-K@users.noreply.github.com>
2023-01-14 19:36:21 +0530

    Add files via upload

---
+++ Mad Libs Generator.py
@@ -0,0 +1,56 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2638
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 """ Mad Libs Generator\
+\
+----------------------------------------\
+\
+"""\
+\
+#Loop back to this point once code finishes\
+\
+loop = 1\
+\
+while (loop < 10):\
+\
+# All the questions that the program asks the user\
+\
+noun = input("Choose a noun: ")\
+\
+p_noun = input("Choose a plural noun: ")\
+\
+noun2 = input("Choose a noun: ")\
+\
+place = input("Name a place: ")\
+\
+adjective = input("Choose an adjective (Describing word): ")\
+\
+noun3 = input("Choose a noun: ")\
+\
+#Displays the story based on the users input\
+\
+print ("------------------------------------------")\
+\
+print ("Be kind to your",noun,"- footed", p_noun)\
+\
+print ("For a duck may be somebody's", noun2,",")\
+\
+print ("Be kind to your",p_noun,"in",place)\
+\
+print ("Where the weather is always",adjective,".")\
+\
+print ()\
+\
+print ("You may think that is this the",noun3,",")\
+\
+print ("Well it is.")\
+\
+print ("------------------------------------------")\
+\
+# Loop back to "loop = 1"\
+\
+loop = loop + 1}
